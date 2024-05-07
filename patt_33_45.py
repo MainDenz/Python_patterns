b29dccd8e4169c7cf6030072fd9d0211eda25846
Denzel Njuki <njukidenzel@gmail.com>
2024-05-07 21:35:59 +0300

    New patt

--- patt_33_45.py
+++ patt_33_45.py
@@ -13,22 +13,25 @@ for i in range(1,6):
         else:
             break
     print()
-
+'''
 #  1
 #  3*  2
 #  4*  5*  6
-# 10*  9*  8*  7
+# 10* 9* 8*  7
 # 11* 12* 13* 14* 15
 print("\nPattern 34")
 s=1
+y=1
 
-'''for i in range(1,6):
-    for j in range(1,6):
-        if j==1:
-            print(s,end="")
-            s+=i
+for i in range(1,6):
+    for j in range(1,i+1):
+        if (i%2==1):
+            print(s,end=" ")
         else:
-            break
+            print(y,end=" ")
+        s = s+1
+    y = y + i - 1
+    print()
 '''
 # #
 # **
@@ -79,4 +82,112 @@ for i in range(1,6):
             break
     print()
 
+
+# 1
+# 1*
+# 1*3
+# 1*3*
+# 1*3*5”
+
+print("\nPattern 38")
+for i in range(1,6):
+    for j in range(1,6):
+        if (j<=i):
+            if(j%2==1):
+                print(j,end="")
+            else:
+                print("*",end="")
+        else:
+            break
+    print()
+
+# 1
+# 2*
+# 3*3
+# 4*4*
+# 5*5*5”
+
+print("\nPattern 39")
+for i in range(1,6):
+    for j in range(1,6):
+        if (j<=i):
+            if(j%2==1):
+                print(i,end="")
+            else:
+                print("*",end="")
+        else:
+            break
+    print()
+
+# #####
+# ****
+# ###
+# **
+# #
+print("\nPattern 40")
+for i in range(6,1,-1):
+    for j in range(1,i):
+        if(i%2==0):
+            print("#",end="")
+        else:
+            print("*",end="")
+    print()
+
+
+# #*#*#
+# #*#*
+# #*#
+# #*
+# #
+print("\nPattern 41")
+for i in range(6,1,-1):
+    for j in range(1,i):
+        if(j%2==1):
+            print("#",end="")
+        else:
+            print("*",end="")
+    print()
+
+# 55555
+# ****
+# 333
+# **
+# 1
+print("\nPattern 42")
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        if(i%2==1):
+            print(i,end="")
+        else:
+            print("*",end="")
+    print()
+
+# 12345
+# ****
+# 123
+# **
+# 1
+print("\nPattern 43")
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        if(i%2==1):
+            print(j,end="")
+        else:
+            print("*",end="")
+    print()
+
+
+# 1*3*5
+# 1*3*
+# 1*3
+# 1*
+# 1”
+print("\nPattern 44")
+for i in range(6,1,-1):
+    for j in range(1,i):
+        if (j%2==1):
+            print(j,end="")
+        else:
+            print("*",end="")
+    print()
 
